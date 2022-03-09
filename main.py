7b8d27e461b2f51d515ec43f91da804c8e6ba791
saravanamuthu1 <41484502+saravanamuthu1@users.noreply.github.com>
2022-03-08 22:58:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from calendar import TUESDAY
 from pip import main
 import streamlit as st
 import pandas as pd
@@ -8,16 +9,19 @@ todrop=[]
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
      # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    dataframe.fillna(0)
-    dataframe.duplicated()
-    st.write(dataframe.describe())
-    st.write("colum name in the csv file")
-    for column in dataframe.columns:
-        st.markdown(column)
-    num_input=st.number_input("enter the number of colums to ve deleted",min_value=0,max_value=len(dataframe.columns),step=1)
-    for i in range(num_input):
-        text_input=st.text_input("enter the column to be deleted")
-        todrop.append(text_input)
-
+    df = pd.read_csv(uploaded_file)
+    df.fillna(0)
+    st.write(df.describe())
+    st.write("check the colomn to be deleted")
+    for column in df.columns:
+        st.checkbox(column)
+        st.line_chart(df[column])
+    st.dataframe(df)
+    text_input=st.text_input("enter the column to e deleted")
+    if(type(text_input) == str):
+        df.drop(columns=[st.text_input("enter the column to be deleted")],axis=1,inplace=True)
+        st.write("column is deleted")
+        st.dataframe(df)
+    else:
+        st.write("column not found")
 
