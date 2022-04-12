8bf1ead1d6ef22919d3c70d6085ba453581168f2
saravanamuthu1 <41484502+saravanamuthu1@users.noreply.github.com>
2022-04-11 21:14:24 -0500

    Update app.py
    deleted unused imports

--- app.py
+++ app.py
@@ -1,37 +1,49 @@
-from ast import Store
-from flask import Flask,render_template,request,url_for,redirect
-from flask_uploads import UploadSet,configure_uploads,IMAGES,DATA,ALL
-from sqlalchemy import column
-from werkzeug.datastructures import  FileStorage
+from fileinput import filename
+from flask import Flask,render_template,url_for,request
+from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet,configure_uploads,ALL,DATA
+from werkzeug import secure_filename
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+# Configuration
+
+files=UploadSet('files',ALL)
+app.config['UPLOADED_FILES_DEST']='static/storage'
+configure_uploads(app,files)
+
 import os
 import datetime
 import time
-import pandas as pd
-import numpy as np
-from dataanalysis import *
-app = Flask(__name__)
-files = UploadSet("files",ALL)
-app.config['UPLOADED_FILES_DEST']=' static/storage'
-configure_uploads(app,files)
+
+
+#EDA Packages
+import pandas as pd 
+import numpy as np 
+
+
+# ML Packages
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+	return render_template('index.html')
 
-@app.route('/uploadFiles', methods=['GET','POST'])
-def uploadFiles():
+@app.route('/datauploads', methods=['GET','POST'])
+def datauploads():
     if request.method == 'POST' and 'csv_data' in request.files:
         file = request.files['csv_data']
-        filename=file.filename
+        filename =secure_filename(file.filename)
         file.save(os.path.join('static/storage',filename))
-        df=get_data(os.path.join('static/storage',filename))
-        df=clean_data(df)
-        data_size=get_size(df)
-        getColumnNames=get_columns(df)
-        shape=get_shape(df)
-    return render_template('index.html',filename=filename,data_size=data_size,getColumnNames=getColumnNames,shape=shape,get_columns=get_columns)
+        df=pd.read_csv(os.path.join('static/storage',filename))
+        df_shape=df.shape
+        df_size=df.size
+        df_columns=df.columns
+        df_head=df.head(10)
+    return render_template('details.html',filename=filename,df_table=df,df_shape=df_shape,df_size=df_size,df_columns=df_columns,df_head=df_head)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+	app.run(debug=True)
 
